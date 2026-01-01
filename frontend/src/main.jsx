981a0ce1af2bc6c8ddadce04949ccac457edb865
Rayyan Khattak <linkcode25@gmail.com>
2026-01-01 22:53:10 +0500

    Added routing to the app

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,44 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Signup from "./pages/Signup.jsx";
+import Login from "./pages/Login.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
+import CreateGroup from "./pages/CreateGroup.jsx";
+import Group from "./pages/Group.jsx";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home />,
+    },
+    {
+        path: "/signup",
+        element: <Signup />,
+    },
+    {
+        path: "/login",
+        element: <Login />,
+    },
+    {
+        path: "/dashboard",
+        element: <Dashboard />,
+    },
+    {
+        path: "/create_group",
+        element: <CreateGroup />,
+    },
+    {
+        path: "/group",
+        element: <Group/>
+    }
+])
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
